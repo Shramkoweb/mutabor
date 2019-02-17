f09ce1991d7cebef44c0a3e893074d6792b121c4
Sergey Shramko <shramko.web@yahoo.com>
2019-02-17 17:11:29 +0200

    Updates

--- js/script.js
+++ js/script.js
@@ -1,1 +1,118 @@
-"use strict";var _typeof="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e},navMain=document.querySelector(".site-navigation"),navToggle=document.querySelector(".toggle"),overlay=document.querySelector(".overlay"),linkModal=document.querySelectorAll(".site-navigation__item"),menuModal=document.querySelectorAll(".navigation-modal");if(navMain.classList.remove("site-navigation--no-js"),navMain.classList.add("site-navigation--closed"),navToggle.addEventListener("click",function(){navToggle.classList.toggle("is-active"),navMain.classList.toggle("site-navigation--closed"),overlay.classList.toggle("overlay--open")}),0<linkModal.length)for(var j=0;j<linkModal.length;j++)linkModal[j].addEventListener("click",function(){this.querySelector(".navigation-modal").classList.toggle("navigation-modal--open")});navMain=document.querySelector(".site-navigation"),navToggle=document.querySelector(".toggle"),overlay=document.querySelector(".overlay"),linkModal=document.querySelectorAll(".site-navigation__item"),menuModal=document.querySelectorAll(".navigation-modal");if(navMain.classList.remove("site-navigation--no-js"),navMain.classList.add("site-navigation--closed"),navToggle.addEventListener("click",function(){navToggle.classList.toggle("is-active"),navMain.classList.toggle("site-navigation--closed"),overlay.classList.toggle("overlay--open")}),0<linkModal.length)for(j=0;j<linkModal.length;j++)linkModal[j].addEventListener("click",function(){this.querySelector(".navigation-modal").classList.toggle("navigation-modal--open")});var filterFns={greaterThan50:function(e){var t=e.querySelector(".number").textContent;return 50<parseInt(t,10)},even:function(e){var t=e.querySelector(".number").textContent;return parseInt(t,10)%2==0}},filters={},iso=new Isotope(".grid",{itemSelector:".grid-item",percentPosition:!0,masonry:{columnWidth:".grid-sizer",gutter:".gutter-sizer",horizontalOrder:!0},filter:function(e){var t=!0;for(var o in filters){var n=filters[o];n=filterFns[n]||n;var i=void 0===n?"undefined":_typeof(n);if(n&&"function"==i?t=n(e):n&&(t=matchesSelector(e,n)),!t)break}return t}});document.querySelector("#filters").addEventListener("click",function(e){if(matchesSelector(e.target,"button")){var t=e.target.parentNode.getAttribute("data-filter-group");filters[t]=e.target.getAttribute("data-filter"),iso.arrange()}});for(var buttonGroups=document.querySelectorAll(".button-group"),i=0;i<buttonGroups.length;i++){var buttonGroup=buttonGroups[i];radioButtonGroup(buttonGroup)}function radioButtonGroup(t){t.addEventListener("click",function(e){matchesSelector(e.target,"button")&&(t.querySelector(".is-checked").classList.remove("is-checked"),e.target.classList.add("is-checked"))})}
+var navMain = document.querySelector(".site-navigation");
+var navToggle = document.querySelector(".toggle");
+var overlay = document.querySelector(".overlay");
+var linkModal = document.querySelectorAll(".site-navigation__item");
+var menuModal = document.querySelectorAll(".navigation-modal");
+
+
+//Открытие меню в мобильно версии.
+
+navMain.classList.remove("site-navigation--no-js");
+navMain.classList.add("site-navigation--closed");
+
+navToggle.addEventListener("click", function () {
+  navToggle.classList.toggle("is-active");
+  navMain.classList.toggle("site-navigation--closed");
+  overlay.classList.toggle("overlay--open");
+})
+
+// modal menu
+
+if (0 < linkModal.length) {
+  for (var j = 0; j < linkModal.length; j++)
+    linkModal[j].addEventListener("click", function () {
+      this.querySelector(".navigation-modal").classList.toggle("navigation-modal--open");
+    })
+};
+
+// filter functions
+var filterFns = {
+  greaterThan50: function (itemElem) {
+    var number = itemElem.querySelector('.number').textContent;
+    return parseInt(number, 10) > 50;
+  },
+  even: function (itemElem) {
+    var number = itemElem.querySelector('.number').textContent;
+    return parseInt(number, 10) % 2 === 0;
+  }
+};
+
+// external js: isotope.pkgd.js, imagesloaded.pkgd.js
+
+var grid = document.querySelector('.grid');
+var iso;
+
+imagesLoaded(grid, function () {
+  // init Isotope after all images have loaded
+  iso = new Isotope('.grid', {
+    itemSelector: '.grid-item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.grid-sizer',
+      gutter: '.gutter-sizer',
+      horizontalOrder: true
+    },
+
+    filter: function (itemElem) {
+
+      var isMatched = true;
+
+      for (var prop in filters) {
+        var filter = filters[prop];
+        // use function if it matches
+        filter = filterFns[filter] || filter;
+        // test each filter
+        var filterType = typeof filter;
+        if (filter && filterType == 'function') {
+          isMatched = filter(itemElem);
+        } else if (filter) {
+          isMatched = matchesSelector(itemElem, filter);
+        }
+        // break if not matched
+        if (!isMatched) {
+          break;
+        }
+      }
+      return isMatched;
+    }
+
+  })
+});
+
+// store filter for each group
+var filters = {};
+
+// init Isotope
+
+document.querySelector('#filters').addEventListener('click', function (event) {
+  // only work with buttons
+  if (!matchesSelector(event.target, 'button')) {
+    return;
+  }
+  // get group key
+  var buttonGroup = event.target.parentNode;
+  var filterGroup = buttonGroup.getAttribute('data-filter-group');
+  // set filter for group
+  filters[filterGroup] = event.target.getAttribute('data-filter');
+  // arrange, and use filter fn
+  iso.arrange();
+});
+
+// change is-checked class on buttons
+var buttonGroups = document.querySelectorAll('.button-group');
+for (var i = 0; i < buttonGroups.length; i++) {
+  var buttonGroup = buttonGroups[i];
+  radioButtonGroup(buttonGroup);
+}
+
+function radioButtonGroup(buttonGroup) {
+  buttonGroup.addEventListener('click', function (event) {
+    // only work with buttons
+    if (!matchesSelector(event.target, 'button')) {
+      return;
+    }
+    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
+    event.target.classList.add('is-checked');
+  });
+}
+
